c4fb4bcd6c90d60353ecd651daf4d8ac3f16e683
J.D. Bean <jonathandbean@gmail.com>
2018-12-04 16:58:20 -0500

    configuring PWA functionality, minor tweak to max-scale viewport tag for a for a11y

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.readyState == "interactive") {
+    /**
+     * Notify when site updates
+     */
+
+    window.addEventListener('sw.update', function() {
+      console.log("UPDATED")
+      var updateToast = document.querySelector('.update-toast');
+      toggleClass(updateToast, 'show');
+    });
+
+    document.querySelector('.update-toast').addEventListener('click', function() {
+        window.location.reload();
+    });
+  }
+});
+
+function toggleClass(element, toggleClass){
+ var currentClass = element.className;
+ var newClass;
+ if(currentClass.split(" ").indexOf(toggleClass) > -1){ //has class
+    newClass = currentClass.replace(new RegExp('\\b'+toggleClass+'\\b','g'),"")
+ }else{
+    newClass = currentClass + " " + toggleClass;
+ }
+ element.className = newClass.trim();
+}
